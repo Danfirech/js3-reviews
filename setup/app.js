3df54606a0c70965e365c15d198c18b92c75bbee
Dan Possehl <danielbpossehl@gmail.com>
2021-12-01 14:22:51 -0700

    added if statments to next and prev functions

--- setup/app.js
+++ setup/app.js
@@ -64,10 +64,18 @@ function showPerson(person) {
 
 nextBtn.addEventListener("click", function () {
   currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
   showPerson(currentItem);
 });
 
+//show prev person
+
 prevBtn.addEventListener("click", function () {
   currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
   showPerson(currentItem);
 });
